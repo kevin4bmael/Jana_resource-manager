84747210ede9f3474edaff3af379b23650d5b876
carvalhoTG <thiagogames1404@gmail.com>
2025-12-04 18:31:25 -0300

    Update Registro.java

---
+++ src/main/java/com/jana/model/Registro.java
@@ -0,0 +1,61 @@
+package main.java.com.jana.model;
+
+import java.util.Date;
+
+public class Registro {
+
+    private int registroId;
+    private int reserveId;
+    private int userId;
+    private int resourceId;
+    private Date dataRetirada;
+    private Date dataDevolucao;
+
+    public int getRegistroId() {
+        return registroId;
+    }
+
+    public void setRegistroId(int registroId) {
+        this.registroId = registroId;
+    }
+
+    public int getReserveId() {
+        return reserveId;
+    }
+
+    public void setReserveId(int reserveId) {
+        this.reserveId = reserveId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public Date getDataRetirada() {
+        return dataRetirada;
+    }
+
+    public void setDataRetirada(Date dataRetirada) {
+        this.dataRetirada = dataRetirada;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+}
